422a65b6a7345c28a3c3bb7a932194f027d334de
HanpengChen <hanpengchen07@163.com>
2021-01-24 09:33:12 +0800

    处理render函数

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import { compileToFunction } from "./compiler";
 import { initState } from "./state";
 
 export function initMixin(Vue) {
@@ -10,5 +11,31 @@ export function initMixin(Vue) {
 
     // 对数据进行初始化 watch props data methods computed
     initState(vm);
+
+    if (vm.$options.el) {
+      // 将数据挂载到这个模板上
+      vm.$mount(vm.$options.el)
+    }
   };
+
+  Vue.prototype.$mount = function(el) {
+
+     const vm = this;
+     const options = vm.$options
+
+    el = document.querySelector(el);
+    // console.log(el)
+
+    // 这里如果直接用正则去替换真实DOM中的数据，太消耗性能
+    // 思路：将模板转化成 对应的渲染函数 =》 产生虚拟dom =》 利用diff算法更新虚拟dom =》 最后产生真实dom
+    if (!options.render) { // 如果没有渲染函数
+      let template = options.template
+      if (!template && el) { // 没有模板但是有el
+        template = el.outerHTML
+      }
+      let render = compileToFunction(template)
+      options.render = render
+    }
+    // 此时options.render就是渲染函数
+  }
 }
