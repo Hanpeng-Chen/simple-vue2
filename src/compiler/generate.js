913bf8acea64175d330cb3a0bab021d1ac61f5ac
HanpengChen <hanpengchen07@163.com>
2021-01-26 00:12:30 +0800

    codegen生成对应代码

---
+++ src/compiler/generate.js
@@ -0,0 +1,72 @@
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; // {{aaaaa}}
+
+function genProps(attrs) {
+  let str = "";
+  for (let i = 0; i < attrs.length; i++) {
+    let attr = attrs[i];
+    // 针对style这个属性进行特殊处理，将其值转为对象
+    if (attr.name === "style") {
+      let obj = {};
+      // attr.value.split(';').forEach(item => {
+      //   let [key, value] = item.split(':')
+      //   obj[key] = value
+      // });
+
+      // 正则匹配
+      attr.value.replace(/([^;:]+)\:([^;:]+)/g, function () {
+        obj[arguments[1]] = arguments[2];
+      });
+      attr.value = obj;
+    }
+    str += `${attr.name}:${JSON.stringify(attr.value)},`;
+  }
+  return `{${str.slice(0, -1)}}`;
+}
+
+function gen(el) {
+  if (el.type === 1) {
+    return generate(el);
+  } else {
+    // 处理本文
+    let text = el.text;
+    if (!defaultTagRE.test(text)) {
+      return `_v('${text}')`;
+    }
+    let lastIndex = (defaultTagRE.lastIndex = 0); // 重置正则为空
+    let tokens = [];
+    let match;
+    while ((match = defaultTagRE.exec(text))) {
+      let index = match.index; //开始索引
+      if (index > lastIndex) {
+        tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+      }
+      tokens.push(`_s(${match[1].trim()})`); // 有可能是对象，等同于JSON.stringify
+      lastIndex = index + match[0].length;
+    }
+    if (lastIndex < text.length) {
+      tokens.push(JSON.stringify(text.slice(lastIndex)));
+    }
+
+    return `_v(${tokens.join("+")})`;
+  }
+}
+
+function genChildren(el) {
+  let children = el.children;
+  if (children) {
+    return children.map((c) => gen(c)).join(",");
+  }
+  return false;
+}
+
+export function generate(el) {
+  // _c('div',{style:{color:'red'}},_v('hello'+_s(name)),_c('span',undefined,''))
+  // 本质就是遍历树，拼接成字符串
+
+  let children = genChildren(el);
+  let code = `_c('${el.tag}', ${
+    el.attrs.length ? genProps(el.attrs) : "undefined"
+  }, ${children ? `,${children}` : ""})`;
+
+  return code;
+}
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,8 +1,9 @@
+import { generate } from "./generate";
 import { parseHTML } from "./parser";
 
-// const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`; // 标签名 
+// const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`; // 标签名
 // const qnameCapture = `((?:${ncname}\\:)?${ncname})`; //  用来获取的标签名的 match后的索引为1的
-// const startTagOpen = new RegExp(`^<${qnameCapture}`); // 匹配开始标签的 
+// const startTagOpen = new RegExp(`^<${qnameCapture}`); // 匹配开始标签的
 // const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`); // 匹配闭合标签的
 // //           aa  =   "  xxx "  | '  xxxx '  | xxx
 // const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/; // a=b  a="b"  a='b'
@@ -11,5 +12,9 @@ import { parseHTML } from "./parser";
 
 export function compileToFunction(template) {
   let root = parseHTML(template);
-  console.log('root', root)
+  // console.log('root', root)
+
+  // 生成代码
+  let code = generate(root);
+  console.log(code);
 }
