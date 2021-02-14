56ded8735c539b5848cb328ebc5b4de91603ab7f
HanpengChen <hanpengchen07@163.com>
2021-02-14 21:34:54 +0800

    组件初始化合并生命周期钩子函数，beforeCreate、created钩子的调用实现

--- src/init.js
+++ src/init.js
@@ -1,6 +1,7 @@
 import { compileToFunction } from "./compiler";
-import { mountComponent } from "./lifecycle";
+import { callHook, mountComponent } from "./lifecycle";
 import { initState } from "./state";
+import { mergeOptions } from "./utils";
 
 export function initMixin(Vue) {
   // 表示在Vue的基础上做一次混合操作
@@ -8,21 +9,22 @@ export function initMixin(Vue) {
     // console.log(options);
     // options中有el、data、props、methods等数据
     const vm = this;
-    vm.$options = options; // 后面还会对options进行扩展操作
+    vm.$options = mergeOptions(vm.constructor.options, options); // 后面还会对options进行扩展操作
 
+    callHook(vm, "beforeCreate");
     // 对数据进行初始化 watch props data methods computed
     initState(vm);
+    callHook(vm, "created");
 
     if (vm.$options.el) {
       // 将数据挂载到这个模板上
-      vm.$mount(vm.$options.el)
+      vm.$mount(vm.$options.el);
     }
   };
 
-  Vue.prototype.$mount = function(el) {
-
+  Vue.prototype.$mount = function (el) {
     const vm = this;
-    const options = vm.$options
+    const options = vm.$options;
 
     el = document.querySelector(el);
     vm.$el = el;
@@ -30,17 +32,19 @@ export function initMixin(Vue) {
 
     // 这里如果直接用正则去替换真实DOM中的数据，太消耗性能
     // 思路：将模板转化成 对应的渲染函数 =》 产生虚拟dom =》 利用diff算法更新虚拟dom =》 最后产生真实dom
-    if (!options.render) { // 如果没有渲染函数
-      let template = options.template
-      if (!template && el) { // 没有模板但是有el
-        template = el.outerHTML
+    if (!options.render) {
+      // 如果没有渲染函数
+      let template = options.template;
+      if (!template && el) {
+        // 没有模板但是有el
+        template = el.outerHTML;
       }
-      let render = compileToFunction(template)
-      options.render = render
+      let render = compileToFunction(template);
+      options.render = render;
     }
     // 此时options.render就是渲染函数
 
     // 组件挂载
-    mountComponent(vm, el)
-  }
+    mountComponent(vm, el);
+  };
 }
--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,31 +1,45 @@
-import Watcher from "./observer/watcher"
-import { nextTick } from "./utils"
-import { patch } from "./vdom/patch"
+import Watcher from "./observer/watcher";
+import { nextTick } from "./utils";
+import { patch } from "./vdom/patch";
 
 export function lifecycleMixin(Vue) {
-  Vue.prototype._update = function (vnode){
+  Vue.prototype._update = function (vnode) {
     // console.log('vnode', vnode)
 
     // 这里既有初始化，又有更新
-    const vm = this
-    vm.$el = patch(vm.$el, vnode) // 如果没有将path后的结果重新赋值给vm.$el，在第一次渲染后最开始的$el已经被删除，后续更新会报找不到老节点
-  }
+    const vm = this;
+    vm.$el = patch(vm.$el, vnode); // 如果没有将path后的结果重新赋值给vm.$el，在第一次渲染后最开始的$el已经被删除，后续更新会报找不到老节点
+  };
 
   Vue.prototype.$nextTick = nextTick;
 }
 
-
 export function mountComponent(vm, el) {
-
-  let updateComponent = () => { // 后续更新可以调用updateComponent方法
+  let updateComponent = () => {
+    // 后续更新可以调用updateComponent方法
     // 调用render函数，生成虚拟dom
-    vm._update(vm._render())
+    vm._update(vm._render());
 
     // 用虚拟dom生成真实dom
-  }
+  };
 
+  callHook(vm, 'beforeMount');
   // updateComponent();
-  new Watcher(vm, updateComponent, ()=> {
-    console.log('更新视图')
-  }, true) // 它是一个渲染watcher，后续会有其他的watcher
+  new Watcher(
+    vm,
+    updateComponent,
+    () => {
+      console.log("更新视图");
+    },
+    true
+  ); // 它是一个渲染watcher，后续会有其他的watcher
+}
+
+export function callHook(vm, hook) {
+  let handlers = vm.$options[hook];
+  if (handlers) {
+    for (let i = 0; i < handlers.length; i++) {
+      handlers[i].call(vm);
+    }
+  }
 }
