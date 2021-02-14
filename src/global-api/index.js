1ec211d772c94b04ae74be0cbecab08552813a6b
HanpengChen <hanpengchen07@163.com>
2021-02-15 00:04:07 +0800

    组件的定义，和mixin合并有问题，待解决

--- src/global-api/index.js
+++ src/global-api/index.js
@@ -1,4 +1,4 @@
-import {mergeOptions} from '../utils'
+import { mergeOptions } from "../utils";
 
 export function initGlobalApi(Vue) {
   // 用来存放全局的配置，每个组件初始化的时候都会和options选项进行合并
@@ -7,5 +7,33 @@ export function initGlobalApi(Vue) {
   Vue.mixin = function (options) {
     this.options = mergeOptions(this.options, options);
     return this; // 支持链式调用
+  };
+
+  // _base 就是Vue的构造函数
+  Vue.options._base = Vue; // 无论后续创建多少个子类，都可以通过_base找到Vue
+  Vue.options.components = {};
+  Vue.component = function (id, definition) {
+    // 保证组件的隔离，每个组件都会产生一个新的类，去继承父类
+    console.log(this.options)
+    definition = this.options._base.extend(definition);
+    this.options.components[id] = definition;
+    console.log(Vue.options.components)
+  };
+
+
+  // 产生一个继承于Vue的类，并且身上应该有父类的所有功能
+  Vue.extend = function (opts) {
+    const Super = this;
+    const Sub = function VueComponent() {
+      this._init()
+    };
+
+    // 原型继承
+    Sub.prototype = Object.create(Super.prototype);
+    // 需要重写constructor，否则它指向的是父类
+    Sub.prototype.constructor = Sub;
+    // options合并，让子类也包含全局的options
+    Sub.options = mergeOptions(Super.options, opts)
+    return Sub;
   };
 }
--- src/utils.js
+++ src/utils.js
@@ -81,6 +81,18 @@ lifeCycleHooks.forEach((hook) => {
   strats[hook] = mergeHook;
 });
 
+strats.components = function (parentVal, childVal) {
+  // 根据父对象构造一个新对象  options.__proto__
+  let options = Object.create(parentVal);
+
+  if (childVal) {
+    for (let key in childVal) {
+      options[key] = childVal[key];
+    }
+  }
+  return options;
+};
+
 export function mergeOptions(parent, child) {
   const options = {};
   for (let key in parent) {
